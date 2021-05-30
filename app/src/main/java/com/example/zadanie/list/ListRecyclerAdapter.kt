e799ac83143d53def0ec0f1e08763ebbccd5dce7
Patrycja Ładowska <psosnowska93@wp.pl>
2021-05-31 00:25:34 +0200

    Recycler fixups

--- app/src/main/java/com/example/zadanie/list/ListRecyclerAdapter.kt
+++ app/src/main/java/com/example/zadanie/list/ListRecyclerAdapter.kt
@@ -1,29 +1,33 @@
 package com.example.zadanie.list
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.zadanie.R
+import com.example.zadanie.databinding.ListItemBinding
 import com.example.zadanie.networking.ListItem
 
 class ListRecyclerAdapter(private val dataSet: List<ListItem>) :
     RecyclerView.Adapter<ListRecyclerAdapter.ViewHolder>() {
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val textView: TextView = view.findViewById(R.id.list_item_title)
+    class ViewHolder(
+        itemsBinding: ListItemBinding
+    ) : RecyclerView.ViewHolder(itemsBinding.root) {
+        val title = itemsBinding.listItemTitle
+        val description = itemsBinding.listItemDescription
+        val imageUrl = itemsBinding.listItemImage
+        val date = itemsBinding.listItemDate
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.list_item, viewGroup, false)
-
-        return ViewHolder(view)
+        val inflater = LayoutInflater.from(viewGroup.context)
+        val itemsBinding = ListItemBinding.inflate(inflater, viewGroup, false)
+        return ViewHolder(itemsBinding)
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        viewHolder.textView.text = dataSet[position].title
+        viewHolder.title.text = dataSet[position].title
+        viewHolder.description.text = dataSet[position].description
+        viewHolder.date.text = dataSet[position].modificationDate
     }
 
     override fun getItemCount() = dataSet.size
